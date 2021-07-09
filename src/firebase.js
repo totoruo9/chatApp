06935c70aaab30cee894c051b5a1c167a9b6488e
Jin Hyuk <wlsgurrla10@naver.com>
2021-07-10 02:27:38 +0900

    [.env] env 파일을 생성해 firebase의 키 값 들을 변경해주었다. .gitignore에 .env를 넣어 git에서는 해당 키 값들이 보이지 않게 해주었다. 위와 같은 코딩을 진행하는 이유는 git에서의 키 값들의 노출을 막기 위함이고, 실질적으로 서버에서 동작할 때는 해당 key 값들은 코드로서 보여지기 때문에 완벽한 보안이라고 할 수는 없다. 위 작업은 그저 git을 위한 작업이었다.

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,13 @@ import firebase from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyA2XWKLa-sguAphRWxvRWzAHV9cg_OuEP4",
-    authDomain: "chatapp-76b49.firebaseapp.com",
-    projectId: "chatapp-76b49",
-    storageBucket: "chatapp-76b49.appspot.com",
-    messagingSenderId: "241054459330",
-    appId: "1:241054459330:web:b4da5ed7f47d4d758de25b"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase 
