ec5da7884a34303b81ef37c07622573096c96f9d
Jin Hyuk <wlsgurrla10@naver.com>
2021-07-10 17:28:07 +0900

    [Login] login 기능 구현, google, github 로그인 구현

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,26 @@
-import React from 'react';
-import AppRouter from './Router';
+import React, { useEffect, useState } from 'react';
+import AppRouter from 'components/Router';
+import {authService} from "fbase";
 
 function App() {
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    authService.onAuthStateChanged((user)=>{
+      if(user){
+        setIsLoggedIn(true);
+      }else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, [])
   return (
-    <AppRouter />
+    <>
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
+      <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+    </>
+    
   );
 }
 
