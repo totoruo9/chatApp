cb55b15d2257f28c2084acbd5ce560d0b2fb95ea
Jin Hyuk <wlsgurrla10@naver.com>
2021-07-10 20:18:42 +0900

    [Login, Log out, profile, navigation] navigation을 통해 홈과 프로필을 이동할 수 있고 프로필에서 로그아웃이 가능하며 로그아웃 시 홈으로 redirct 시켜준다.

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,19 +1,31 @@
-import React, { useState } from 'react';
-import {HashRouter as Router, Route, Switch} from 'react-router-dom';
+import React from 'react';
+import {HashRouter as Router, Redirect, Route, Switch} from 'react-router-dom';
+import Profile from 'routes/Profile';
 import Auth from '../routes/Auth';
 import Home from '../routes/Home';
+import Navigation from './Navigation';
 
 const AppRouter = ({isLoggedIn}) => {
-    
     return (
         <Router>
+            {isLoggedIn && <Navigation />} 
             <Switch>
-                {isLoggedIn ?
-                <>
-                    <Route exact path="/">
-                        <Home />
-                    </Route>
-                </>:<Route exact path="/"> <Auth /> </Route>
+                {isLoggedIn ? (
+                    <>
+                        <Route exact path="/">
+                            <Home />
+                        </Route>
+                        <Route exact path="/profile">
+                            <Profile />
+                        </Route>
+                    </>
+                ):(
+                    <>
+                        <Route exact path="/">
+                            <Auth />
+                        </Route>
+                    </>
+                )
                 }
             </Switch>
         </Router>
