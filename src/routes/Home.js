70f03c131c6a20fbc31e4a62b8ccd2d06bf842c6
Jin Hyuk <wlsgurrla10@naver.com>
2021-07-10 20:56:57 +0900

    [home] db에 저장한 데이터를 가져와서 화면에 그려줌

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,23 @@
 import { dbService } from 'fbase';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
     const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
+    const getNweets = async() => {
+        const dbNweet = await dbService.collection("nweet").get();
+        dbNweet.forEach(document => {
+            const nweetObject = {
+                ...document.data(),
+                id: document.id,
+            }
+            setNweets(prev => [nweetObject, ...prev]);
+        });
+    }
+    console.log(nweets);
+    useEffect(()=>{
+        getNweets();
+    }, [])
     const onSubmit = async (e) => {
         e.preventDefault();
         await dbService.collection("nweet").add({
@@ -21,6 +36,13 @@ const Home = () => {
                 <input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
                 <input type="submit" value="enter" />
             </form>
+            <div>
+                {nweets.map(nweet => (
+                    <div key={nweet.id}>
+                        <h4>{nweet.nweet}</h4>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 };
