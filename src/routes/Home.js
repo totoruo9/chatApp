0897df29cba83a5a64a611fffcb9867898d8c234
Jin Hyuk <wlsgurrla10@naver.com>
2021-07-10 20:26:45 +0900

    [home, database] datebase 생성, 홈 form 영역 생성

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+    const [nweet, setNweet] = useState("");
+    const onSubmit = (e) => {
+        e.preventDefault();
+    };
+    const onChange = (e) => {
+        const {target:{value}} = e;
+        setNweet(value);
+    }
+    return(
+        <div>
+            <form onSubmit={onSubmit}>
+                <input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
+                <input type="submit" value="enter" />
+            </form>
+        </div>
+    )
+};
+
 export default Home;
