635429570a07f8c3aaa56fc658eccbb97542905d
Jin Hyuk <wlsgurrla10@naver.com>
2021-07-11 21:40:29 +0900

    [img file upload] 이미지 파일을 불러와서 화면에 미리보기 이미지로 보여준다

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,6 +5,7 @@ import Nweet from "../components/Nweet";
 const Home = ({userObj}) => {
     const [nweet, setNweet] = useState("");
     const [nweets, setNweets] = useState([]);
+    const [attachment, setAttachment] = useState();
     useEffect(()=>{
         dbService.collection("nweets").onSnapshot(snapshot => {
             const nweetArray = snapshot.docs.map(doc => ({id:doc.id, ...doc.data()}));
@@ -23,12 +24,28 @@ const Home = ({userObj}) => {
     const onChange = (e) => {
         const {target:{value}} = e;
         setNweet(value);
-    }
+    };
+    const onFileChange = (e) => {
+        const {target:{files}} = e;
+        const theFile = files[0];
+        const reader = new FileReader();
+        reader.onloadend = (finishedEvent) => {
+            const {currentTarget: {result}} = finishedEvent
+            setAttachment(result);
+        }
+        reader.readAsDataURL(theFile);
+    };
+    const onClearAttachment = () => setAttachment(null);
     return(
         <div>
             <form onSubmit={onSubmit}>
                 <input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
+                <input type="file" accept="image/*" onChange={onFileChange} />
                 <input type="submit" value="enter" />
+                {attachment && <div>
+                    <img src={attachment} width="50px" height="50px" />
+                    <button onClick={onClearAttachment}>Clear</button>
+                </div>}
             </form>
             <div>
                 {nweets.map(nweet => (
