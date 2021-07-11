3d29eb64cb79f028e3593953ad74259bfbf64094
Jin Hyuk <wlsgurrla10@naver.com>
2021-07-11 22:15:07 +0900

    [file delete] 업로드한 파일을 삭제하는 기능 구현(화면에서만 삭제하는 것이 아닌 storage와 db에서 삭제 시킨다.)

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -16,7 +16,7 @@ const Home = ({userObj}) => {
     const onSubmit = async (e) => {
         e.preventDefault();
         let attachmentUrl = "";
-        if(attachment){
+        if(attachment !== ""){
             const attachmentRef = storageService.ref().child(`${userObj.uid}/${uuidv4()}`);
             const response = await attachmentRef.putString(attachment, "data_url");
             attachmentUrl = await response.ref.getDownloadURL();
